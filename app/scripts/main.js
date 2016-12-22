3c0d853c604748d0c8c13952435c6d2068d9cb6a
Thibaut Leger <thibautleger@thibauts-imac.lan>
2016-12-22 17:10:14 +0000

    slick

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,18 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function(){
+  $('.more-products').slick({
+    centerMode: true,
+	  centerPadding: '60px',
+	  slidesToShow: 3,
+	  responsive: [
+	    {
+	      breakpoint: 768,
+	      settings: {
+	        arrows: false,
+	        centerMode: true,
+	        centerPadding: '30px',
+	        slidesToShow: 1
+	      }
+	    }
+	  ]
+  });
+});
